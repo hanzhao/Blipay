ddc15a750fb1506f7d819dffc5f5632882426164
Magica <readme.md@gmail.com>
2016-05-04 20:58:11 +0800

    Add auto deployment

--- config/production.js
+++ config/production.js
@@ -1,5 +1,17 @@
 module.exports = {
   cookie: {
-    secret: '34jX04l2_mrxoh'
+    secret: 'Wxdf3_2Pmzrxfa'
+  },
+  database: {
+    db: 'blipay',
+    username: 'blipay',
+    password: 'blipay',
+    dialect: 'mysql',
+    host: 'localhost',
+    pool: {
+      min: 0,
+      max: 10,
+      idle: 1000
+    }
   }
 };
