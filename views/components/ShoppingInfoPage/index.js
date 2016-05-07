c08f6fd3947200e596f320eb874519e9b9118246
bohanli <libohan@zju.edu.cn>
2016-05-07 23:53:34 +0800

    modify shopping pages

--- views/components/ShoppingInfoPage/index.js
+++ views/components/ShoppingInfoPage/index.js
@@ -5,7 +5,9 @@ import { Cascader } from 'antd';
 import { reduxForm } from 'redux-form';
 import { Form, Input, Checkbox } from 'antd';
 import { Pagination } from 'antd';
+import styles from './styles';
 import pic from './gg.png'
+
 const optionsPrice = [{
   value: 'priceLowToHigh',
   label: '价格从小到大'
@@ -43,26 +45,28 @@ class ShoppingInfoPage extends React.Component {
    		searchString
     }, handleSubmit } = this.props;
     return (
-   	<div>
-        <Cascader placeholder="请选择排序类型" options={optionsPrice} onChange={onChange} />
-        <Cascader placeholder="请选择商品类别" options={optionsCategory} onChange={onChange} />
-        <searchItem
-          label="账户：">
-          <Input placeholder="请输入关键字"
-            {...searchString} />
-        </searchItem>
-        <img src={pic} />  <h1>Hai daren</h1> <p>admin</p> <p>100$</p>
-        <hr />
-        <img src={pic} />  <h1>Hai daren</h1> <p>admin</p> <p>100$</p>
-        <hr />
-        <img src={pic} />  <h1>Hai daren</h1> <p>admin</p> <p>100$</p>
-        <hr />
-        <img src={pic} />  <h1>Hai daren</h1> <p>admin</p> <p>100$</p>
-        <hr />
-        <img src={pic} />  <h1>Hai daren</h1> <p>admin</p> <p>100$</p>
-        <hr />
-        <img src={pic} />  <h1>Hai daren</h1> <p>admin</p> <p>100$</p>
-        <hr />
+   	<div className={styles.container}>
+		<div className={styles.upperHalf}>
+			<nobr>
+        	<Cascader className={styles.cascader} placeholder="请选择排序类型" options={optionsPrice} onChange={onChange} />
+        	<Cascader className={styles.cascader} placeholder="请选择商品类别" options={optionsCategory} onChange={onChange} />
+		      <Input className={styles.inputbox} placeholder="请输入关键字" {...searchString} />
+					<Button type="ghost" className={styles.button}>搜索</Button>
+			</nobr>
+		</div>
+
+
+		<div className={styles.lowerHalf}>
+			<div className={styles.itemRecord}>
+		    	<img src={pic} className={styles.itemImage}/>  
+					<nobr>
+					<h1>Hai daren</h1> <p>admin</p> <p>100$</p>
+					</nobr>
+			</div>
+
+		</div>
+	
+
         <Pagination simple defaultCurrent={2} total={12450} />
     </div>
     );
--- views/components/ShoppingPage/index.js
+++ views/components/ShoppingPage/index.js
@@ -15,14 +15,14 @@ function onChange(value) {
 class ShoppingPage extends React.Component {
   render() {
   	return(
-      <Container className={styles.container}>
-        <div className={styles.left}>
-        	<ShoppingMenu/>
-        </div>
-        <div className={styles.right}>
-          	{ this.props.children }
-        </div>
-      </Container>
+        <Container className={styles.container}>
+          <div className={styles.left}>
+          	<ShoppingMenu/>
+          </div>
+          <div className={styles.right}>
+            	{ this.props.children }
+          </div>
+        </Container>
     )
   }
 }
