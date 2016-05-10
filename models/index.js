f74e2e013c0c3bf9b5f8192a2546b208da097f5f
zmj1316 <zmj1316@gmail.com>
2016-05-10 19:48:03 +0800

    B2 add models
    for items orders and associations between

--- models/index.js
+++ models/index.js
@@ -10,19 +10,24 @@ const db = new Sequelize(config.db, config.username, config.password, {
 const User = require('./user')(db);
 const Item = require('./item')(db);
 const Transaction = require('./transaction')(db);
-
+const Order = require('./order')(db);
+const OrderItem = require('./orderitem')(db);
 // 表关联
 Item.belongsTo(User, {
   as: 'seller'
 });
 Transaction.belongsTo(User);
 
-[User, Item, Transaction].forEach((t) => {
+Order.belongsTo(User, {as: 'seller'});
+Order.belongsTo(User, {as: 'customer'});
+Order.belongsToMany(Item, {through: OrderItem});
+
+[User, Item, Transaction, Order, OrderItem].forEach((t) => {
   t.sync().then(() => {
     console.log(`Table ${t.name} synced`);
   });
 });
 
 module.exports = {
-  User, Item, Transaction
+  User, Item, Transaction, Order, OrderItem
 };
