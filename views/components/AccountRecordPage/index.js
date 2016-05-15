400ddc23fd863e4e53e5297f7380a0673df4b1f6
Cynric777 <383467294@qq.com>
2016-05-16 03:02:35 +0800

    添加查询记录功能
    尚未从后端获取数据

--- views/components/AccountRecordPage/index.js
+++ views/components/AccountRecordPage/index.js
@@ -2,20 +2,23 @@
  * “个人账户”页面中“交易记录”选项对应的右侧方框。
  */
 import React from 'react';
-
-import { DatePicker, Button } from 'antd';
+import { connect } from 'react-redux';
+import { reduxForm } from 'redux-form';
+import { Form, DatePicker, Button } from 'antd';
 import AccountRecordTable from '../AccountRecordTable';
-
+import { query } from '../../redux/modules/account/query';
+import { getUserId } from '../../redux/modules/account/login';
+import store from '../../redux/store';
 import styles from './styles';
 
 const RangePicker = DatePicker.RangePicker;
-
-const fakeData = Array(50).fill({
-  date: '2015.01.01 19:08:32',
-  description: '账户充值',
-  amount: 100.00,
-  status: '交易成功'
-});
+const userId = getUserId(store.getState());
+//const fakeData = Array(50).fill({
+//  date: '2015.01.01 19:08:32',
+//  description: '账户充值',
+//  amount: 100.00,
+//  status: '交易成功'
+//});
 
 const tableProps = {
   pagination: {
@@ -25,20 +28,42 @@ const tableProps = {
   }
 };
 
+@connect(
+  (state) => ({
+    queryResult: state.account.query.queryResult,
+    errorMsg: state.account.query.errorMsg
+  }), 
+  {
+    query
+  }
+)
+@reduxForm({
+  form: 'user-query',
+  fields: ['chosenDate']
+  }, undefined, {
+  onSubmit: (data) => {
+    store.dispatch(query(userId, data.chosenDate[0], data.chosenDate[1]));
+    //store.dispatch(query(1, data.chosenDate[0], data.chosenDate[1]));
+  }
+})
 class AccountRecordPage extends React.Component {
   render() {
+    const {  
+      fields: {chosenDate}, 
+      handleSubmit} = this.props;
     return (
-      <div className={styles.container}>
-        {/* 日期选择 */}
-        <RangePicker class={styles.picker}/>
-        {/* 搜索按钮 */}
-        <Button type="ghost" className={styles.button}>搜索</Button>
-        <div className={styles.wrapper}>
-          <AccountRecordTable className={styles.table}
-                              data={fakeData}
-                              tableProps={tableProps}/>
+      <Form horizontal onSubmit={handleSubmit}>
+        <div className={styles.container}>
+          <RangePicker class={styles.picker} {...chosenDate}/>
+          <Button type="ghost" className={styles.button} htmlType="submit">搜索</Button>
+          <div className={styles.wrapper}>
+            <AccountRecordTable className={styles.table}
+                                data={this.props.queryResult ? this.props.queryResult : null}
+                                tableProps={tableProps}/>
+          </div>
         </div>
-      </div>);
+      </Form>
+    );
   }
 }
 
