f62593d16e7564514c660878762168cfc23abc7d
fhl <3130103807@zju.edu.cn>
2016-05-25 23:14:56 +0800

    nothing

--- config/admin.js
+++ config/admin.js
@@ -1,9 +1,15 @@
 module.exports = {
     loginSaltPos: 7,
-    paySaltPos: 23,
     Authority: {
         BOOK:       1, //订票服务员
         AUDITOR:    2, //审计员
         SYSTEM:     4  //系统管理员
+    },
+    adminLevel:{
+        NONE:       1, //无权限管理员
+        NORMAL:     2, //普通管理员
+        ROOT:       3  //根管理员
     }
+    LEVELUP: 1      //管理员等级增加
+    LEVELDOWN: -1   //管理员等级降低
 };
