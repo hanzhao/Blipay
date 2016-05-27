e22c1a4a6f951566729cbfe21e0ee81a57cc1be5
CHENXUEER <1165562952@qq.com>
2016-05-27 20:57:27 +0800

    fix bug

--- views/components/AdminVerifyDetail/index.js
+++ views/components/AdminVerifyDetail/index.js
@@ -3,7 +3,9 @@
 */
 import React from 'react';
 import { Link } from 'react-router';
-import { Input, Button, Row,Col, Table,Popconfirm,message} from 'antd';
+import photo from './2233.png';
+import FormModal from '../FormModal';
+import { Input, Button, Row,Col, Table,Popconfirm,message,Form,Modal} from 'antd';
 import styles from './styles';
 
 function confirm() {
@@ -12,6 +14,65 @@ function confirm() {
 
 function cancel() {
   message.error('点击了取消');
+
+}
+
+const Reject = React.createClass({
+  getInitialState() {
+    return {
+      loading: false,
+      visible: false,
+    };
+  },
+  showModal() {
+    this.setState({
+      visible: true,
+    });
+  },
+  handleOk() {
+    this.setState({ loading: true });
+    setTimeout(() => {
+      this.setState({ loading: false, visible: false });
+    }, 3000);
+  },
+  handleCancel() {
+    this.setState({ visible: false });
+  },
+  render() {
+    return (
+      <div>
+        <Button type="primary" onClick={this.showModal}>
+          拒绝
+        </Button>
+        <Modal ref="modal"
+          visible={this.state.visible}
+          title="拒绝验证" onOk={this.handleOk} onCancel={this.handleCancel}
+
+          footer={[
+            <Button key="back" type="ghost" size="large" onClick={this.handleCancel}>返 回</Button>,
+            <Button key="submit" type="primary" size="large" loading={this.state.loading} onClick={this.handleOk}>
+              提 交
+            </Button>,
+          ]} >
+
+          <Input placeholder="请输入拒绝理由" />
+
+        </Modal>
+      </div>
+    );
+  },
+});
+
+const modalconfirm = Modal.confirm;
+function showConfirm_accept() {
+  modalconfirm({
+    title: '确定通过该用户的实名验证吗？',
+
+    onOk() {
+      console.log('确定');
+    },
+    onCancel() {},
+  });
 }
 
 const columns1 = [{
@@ -26,12 +87,6 @@ const columns1 = [{
   title: '用户真实姓名',
   dataIndex: 'realname',
   key: 'realname'
-/* ,  render(text, record) {
-    return (
-      <Button onClick={() => alert(record.id)}>{ text }</Button>
-    )
-  }
-*/
 }
 ];
 
@@ -41,12 +96,19 @@ const columns2 = [{
   key:'identification_front'
   /*,  render(text, record) {
       return (
-        <Button onClick={() => alert(record.id)}>{ text }</Button>
-      )*/
+        <img alt="example" width="10%" src="photo" />
+      )
+    }
+    */
 },{
   title:'身份证反面',
   dataIndex:'identification_back',
   key:'identification_back'
+  /*,  render(text, record) {
+      return (
+        <img alt="example" width="10%" src="photo" />
+      )
+    }*/
 }
 ];
 
@@ -67,13 +129,16 @@ const data1 = [{
 const data2 = [{
   key: '1',
   identification_front:'身份证正面',
-  identification_back:'身份证反面'
+  identification_back:'身份证反面',
+
 }];
 
 const data3 = [{
   key: '1',
   information:'浙江大学2013级学生'
 }];
+
+const datanull=null;
 
 class AdminVerifyDetail extends React.Component {
   render() {
@@ -88,30 +153,42 @@ class AdminVerifyDetail extends React.Component {
         <Table
                className={styles.table}
                columns={columns2}
-               dataSource={data2}
-               pagination={false}
-        />
+
+               pagination={false} //需要有{}
+        >
+        <div>
+          <Row className={styles.juzhong} /*styles={{text-align:"center"}}*/>
+            <Col span={12}>
+              <img alt="example" width="10%" src="photo" />
+            </Col>
+            <Col span={12}>
+              <img alt="example" width="10%" src="photo" />
+            </Col>
+          </Row>
+        </div>
+
+
+        </Table>
+
         <Table
                className={styles.table2}
                columns={columns3}
                dataSource={data3}
                pagination={false}
         />
+
         <div>
           <Row className={styles.juzhong} /*styles={{text-align:"center"}}*/>
             <Col span={8}>
-              <Popconfirm title="确定通过该用户的实名验证吗？" onConfirm={confirm} onCancel={cancel}>
-                <Button><a href="#">通过</a></Button>
-                </Popconfirm>
+              <Button onClick={showConfirm_accept}>通过</Button>
             </Col>
             <Col span={8}>
-              <Popconfirm title="确定要拒绝该用户的实名验证吗？" onConfirm={confirm} onCancel={cancel}>
-                <Button><a href="#">拒绝</a></Button>
-                </Popconfirm>
+                <Reject />
             </Col>
             <Col span={8}><Button><Link to="/admin/account/verification">返回</Link></Button></Col>
           </Row>
         </div>
+
       </div>
     );
   }
@@ -121,4 +198,4 @@ export default AdminVerifyDetail;
 
 //待商榷：点击通过之后可否再点拒绝
 //说明：点击一次通过或拒绝之后，该条用户验证从用户验证列表中消失
-//待完成：调节页面高度，插入身份证正反面图片
+//待完成：调节页面高度，插入身份证正反面图片，onclick改用redux实现
